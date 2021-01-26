2eddb9ccbc39a6473d1767c9bad2302b8cca1cb9
sing3demons <sing3demons@live.com>
2021-01-26 17:30:31 +0700

    init project C#, dotnet core5

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cs_ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string getProducts()
+        {
+            return "Hello";
+        }
+    }
+}
