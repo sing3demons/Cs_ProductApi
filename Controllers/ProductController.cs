eee52df14dbdc49742a0d2431811d9f1df4e814c
sing3demons <sing3demons@live.com>
2021-01-27 17:36:58 +0700

    CRUD product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -27,17 +27,22 @@ namespace Cs_ProductApi.Controllers
 
         // ------> GET: api/values
         [HttpGet]
-        public IActionResult GetProducts()
+        //public IActionResult GetProducts()
+        public async Task<ActionResult<Product>> GetProducts() 
         {
             try
             {
-                IEnumerable<Product> data = DatabaseContext.Products.Include(p => p.ProductCategory).ToList();
-            return Ok(data);
+                //IEnumerable<Product> data = DatabaseContext.Products.Include(p => p.ProductCategory).ToList();
+                //return Ok(data);
+
+                var todoItem = await DatabaseContext.Products.Include(p => p.ProductCategory).ToListAsync();
+
+                return Ok(todoItem);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"GetProducts: {ex.Message}");
-                return BadRequest(ex.Message);
+                return NotFound(ex.Message);
             }
         }
 
@@ -56,42 +61,124 @@ namespace Cs_ProductApi.Controllers
             }
         }
 
-        // GET api/values/5
+        // GET: api/product/1
+        //[HttpGet("{id}", Name = "GetItem")]
+        //public IActionResult CreateProduct([FromBody] Product model)
         [HttpGet("{id}")]
-        public IActionResult Get(int id)
+        public async Task<ActionResult> GetItem(int id)
         {
             try
             {
-                Product product = DatabaseContext.Products.SingleOrDefault(p => p.ID == id);
+                //Product product =  DatabaseContext.Products.SingleOrDefault(p => p.ID == id);
+                var product = await DatabaseContext.Products.FindAsync(id);
                 if (product == null)
                 {
                     return NotFound();
                 }
+
+     
                 return Ok(product);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"GetProduct: {ex.Message}");
-                return BadRequest();
+                return NotFound(ex.Message);
             }
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // POST: api/product
+       [HttpPost]
+        //public IActionResult CreateProduct([FromBody] Product model)
+       public async Task<ActionResult<Product>> CreateProduct([FromBody] Product model)
         {
+            try
+            {
+               
+                //DatabaseContext.Products.Add(model);
+                DatabaseContext.Products.Add(model);
+                //DatabaseContext.SaveChanges();
+                await DatabaseContext.SaveChangesAsync();
+                //return CreatedAtRoute("GetProduct", new { id = 8 }, model);
+                return CreatedAtAction(nameof(GetItem), new { id = model.ID }, model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"CreateProduct: {ex.Message}");
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        public IActionResult UpdateProduct([FromBody] Product model, int id)
+        //public async Task<IActionResult> UpdateProduct(int id, Product model)
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
         {
+            try
+            {
+                var product = DatabaseContext.Products.Find(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                product.Name = model.Name;
+                product.Desc = model.Desc;
+                product.Image = model.Image;
+                product.Price = model.Price;
+                product.CategoryID = model.CategoryID;
+              
+
+                DatabaseContext.Products.Update(product);
+                DatabaseContext.SaveChanges();
+
+                return Ok("success");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"UpdateProduct: {ex.Message}");
+                return BadRequest();
+            }
+
+
+    
+        }
+  
+      
+        // DELETE: api/product/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        //public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var product = DatabaseContext.Products.Find(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                DatabaseContext.Products.Remove(product);
+                DatabaseContext.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"DeleteProduct: {ex.Message}");
+                return BadRequest();
+            }
+            //    var product = await DatabaseContext.Products.FindAsync(id);
+            //    if (product == null)
+            //    {
+            //        return NotFound("Not Found");
+            //    }
+
+            //    DatabaseContext.Products.Remove(product);
+            //    await DatabaseContext.SaveChangesAsync();
+
+            //    return NoContent();
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,31 +1,15 @@
 ﻿using System;
 namespace Cs_ProductApi.Models
 {
-    //public class Product
-    //{
-    //    public int ID { get; set; }
-    //    public string Name { get; set; }
-    //    public string Desc { get; set; }
-    //    public string Image { get; set; }
-    //    public int Price { get; set; }
-    //    public int CategoryID { get; set; } //relation Key
-    //    public Category CategoryName { get; set; }
-    //}
+   
     public class Product
     {
-        //public int ProductID { get; set; }
         public int ID { get; set; }
-
         public string Name { get; set; }
-
         public string Desc { get; set; }
         public string Image { get; set; }
-
         public int Price { get; set; }
-
         public int CategoryID { get; set; } // Relation [Key]
-
-
         public Category ProductCategory { get; set; }
 
       
